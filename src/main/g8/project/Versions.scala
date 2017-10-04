7f088a99aac4125333126c53db47af89f25a56b2
Harikiranvuyyuru <harikiran.vuyyuru@gmail.com>
2017-10-04 14:25:08 -0500

    Changing squbs activator templates to giter8 templates (#10)

---
+++ src/main/g8/project/Versions.scala
@@ -0,0 +1,11 @@
+object Versions {
+  val akkaV = "2.4.20"
+  val akkaHttpV = "10.0.10"
+  val squbsV = "0.9.2"
+  val jacksonV = "2.8.6"
+  val akkaHttpJacksonV = "1.17.0"
+  val junitV = "4.12"
+  val junitInterfaceV = "0.11"
+  val scalaJavaCompatV = "0.7.0"
+  val logbackClassicV = "1.1.3"
+}
