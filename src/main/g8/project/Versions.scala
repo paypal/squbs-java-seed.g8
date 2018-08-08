b37049e0ebdb0716ae9364b986c78ea7b09489be
Anil Gursel <agursel@paypal.com>
2018-08-08 16:53:10 -0500

    Upgrade to squbs 0.11.0

--- src/main/g8/project/Versions.scala
+++ src/main/g8/project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
-  val akkaV = "2.5.11"
-  val akkaHttpV = "10.1.0"
-  val squbsV = "0.10.0"
-  val jacksonV = "2.9.4"
+  val akkaV = "2.5.14"
+  val akkaHttpV = "10.1.3"
+  val squbsV = "0.11.0"
+  val jacksonV = "2.9.5"
   val akkaHttpJacksonV = "1.17.0"
   val junitV = "4.12"
   val junitInterfaceV = "0.11"
